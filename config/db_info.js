492a137b94053be19267d43ed1305fb06e8a412e
Jeonhyeongwoo1 <guddn1234k@gmail.com>
2022-07-12 19:24:19 +0900

    DB 정보 삭제

--- config/db_info.js
+++ config/db_info.js
@@ -1,21 +1,6 @@
 
 module.exports = (
 	function () {
-		return {
-			local: {
-				host: 'battlewithanyoneview.cafe24app.com',
-				user: 'guddn1234k',
-				password: 'wjsguddn1!',
-				database: 'test',
-				port: '3306'
-			},
-			dev: {
-				host: 'battlewithanyoneview.cafe24app.com',
-				user: 'guddn1234k',
-				password: 'wjsguddn1!',
-				database: 'guddn1234k',
-				port: '3306'
-			}
-		}
+		
 	}
 )();
